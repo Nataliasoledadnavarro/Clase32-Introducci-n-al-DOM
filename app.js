94bc561de47014cedc4a03f1a3f04f84bb9f90b2
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-07-17 19:54:30 -0300

    Hasta ejercicio 3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-////EJERCICIO 1////
+////EJERCICIO 1//// en visual da ok pero en mumuki no
 
 /*const nombreUsuario = prompt("¿Cuál es tu nombre?");
 
@@ -7,3 +7,34 @@ const titulo = document.querySelector("h1");
 titulo.textContent = "Hola " + nombreUsuario + ". " + "Te damos la bienvenida.";*/
 
 ////EJERCICIO 2////
+
+/*const ingresarColor = prompt("Ale, por favor ingresá un color en sistema hexadecimal, INCLUYENDO EL #. Gracias!");
+
+const bodyDeColor = document.querySelector("body")
+
+console.log(bodyDeColor.style)
+
+bodyDeColor.style.backgroundColor = ingresarColor*/
+
+////EJERCICIO 3////
+
+/*const primerValor = prompt(
+  "Ale, por favor ingresá el primer valor correspondiendo al color en sistema RGB."
+);
+
+const segundoValor = prompt(
+  "Ale, por favor ingresá el segundo valor correspondiendo al color en sistema RGB."
+);
+
+const tercerValor = prompt(
+  "Ale, por favor ingresá el tercer valor correspondiendo al color en sistema RGB."
+);
+
+const colorRgb =
+  "rgb(" + primerValor + "," + segundoValor + "," + tercerValor + ")";
+
+const colorBody = document.querySelector("body");
+
+colorBody.style.backgroundColor = colorRgb;*/
+
+////EJERCICIO 4////
