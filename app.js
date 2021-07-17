9d753cbcb8320e352d940662e51a35314c413e51
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-07-17 18:57:14 -0300

    Primer ejercicio

---
+++ app.js
@@ -0,0 +1,9 @@
+////EJERCICIO 1////
+
+/*const nombreUsuario = prompt("¿Cuál es tu nombre?");
+
+const titulo = document.querySelector("h1");
+
+titulo.textContent = "Hola " + nombreUsuario + ". " + "Te damos la bienvenida.";*/
+
+////EJERCICIO 2////
